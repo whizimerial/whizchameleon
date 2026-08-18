efcb6d064ff114af69ddc252f59b8f13f2841eb1
whizimerial <whizimerialwhismy@gmail.com>
2026-08-18 14:33:00 +0800

    Update extrashapes.lua

--- extrashapes.lua
+++ extrashapes.lua
@@ -17,5 +17,45 @@ return {
 		collision_box = { type = "regular" },
 		mesh = "whizchameleon_tbpillar.obj",
 	},
+	
+	horizontalpillar = {
+		desc = "Horizontal Pillar",
+		drawtype = "mesh",
+		selection_box = { type = "regular" },
+		collision_box = { type = "regular" },
+		mesh = "whizchameleon_vpillar.obj",
+	},
+	
+	zoomone = {
+		desc = "Zoomed Topright",
+		drawtype = "mesh",
+		selection_box = { type = "regular" },
+		collision_box = { type = "regular" },
+		mesh = "whizchameleon_zoomtr.obj",
+	},
+	
+	zoomtwo = {
+		desc = "Zoomed Topleft",
+		drawtype = "mesh",
+		selection_box = { type = "regular" },
+		collision_box = { type = "regular" },
+		mesh = "whizchameleon_zoomtl.obj",
+	},
+	
+	zoomthree = {
+		desc = "Zoomed Bottomright",
+		drawtype = "mesh",
+		selection_box = { type = "regular" },
+		collision_box = { type = "regular" },
+		mesh = "whizchameleon_zoombr.obj",
+	},
+	
+	zoomfour = {
+		desc = "Zoomed Bottomleft",
+		drawtype = "mesh",
+		selection_box = { type = "regular" },
+		collision_box = { type = "regular" },
+		mesh = "whizchameleon_zoombl.obj",
+	},
 
 }
